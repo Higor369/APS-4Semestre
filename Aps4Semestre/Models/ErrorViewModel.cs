9e0055cf86153f4b4d11444eb27fe9e0819254e4
Higor barbosa <higorpereirabarbosa@gmail.com>
2019-10-26 13:01:10 -0300

    Adicionar arquivos de projeto.

---
+++ Aps4Semestre/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aps4Semestre.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
